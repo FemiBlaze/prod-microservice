818c319cd0763b44a67cefdab043fd7a00135fa1
Blaze <blaze2dtop@gmail.com>
2026-02-19 21:32:33 +0100

    Added GitHub Actions CI/CD pipeline with OIDC integration

---
+++ infra/iam-github.tf
@@ -0,0 +1,66 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+resource "aws_iam_role" "github_actions_role" {
+  name = "github-actions-ecs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:<FemiBlaze/prod-microservice:*"
+        }
+      }
+    }]
+  })
+}
+resource "aws_iam_role_policy" "github_actions_policy" {
+  role = aws_iam_role.github_actions_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:BatchGetImage"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecs:UpdateService",
+          "ecs:DescribeServices"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,4 +5,7 @@ output "ecr_repository_url" {
 output "alb_dns_name" {
   value = aws_lb.app.dns_name
 }
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions_role.arn
+}
 
